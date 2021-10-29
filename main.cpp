0515bce79d5a79e50ccde850efe19ea8881bb332
20181290 HanChanHui <78011635+hanchanhui@users.noreply.github.com>
2021-10-29 02:13:40 +0000

    SDL-Game클래스화

--- main.cpp
+++ main.cpp
@@ -1,47 +1,17 @@
-#include <SDL.h>
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
+#include "Game.h"
+Game* g_game = 0;
 
-bool init(const char* title, int xpos, int ypos, int height, int width, int flags){
-  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-
-        g_pWindow = SDL_CreateWindow(title,xpos,ypos,height,width,flags);
-            
-        if (g_pWindow != 0) {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-
-    }
-    else {
-        return false;
-    }
-    SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
-    return true;
-}
-
-void render(){
-  SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
-}
 
 int main(int argc, char* args[])
 {
-  if(init("Btreaking Up HelloSDL", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
-  640,480,SDL_WINDOW_SHOWN)){
-    g_bRunning= true;
-  }else{
-    return 1;
+  g_game = new Game();
+  g_game->init("Game Class", 100, 100, 640, 480, 0);
+  while(g_game->running()){
+    g_game->handleEvents();
+    g_game->update();
+    g_game->render();
   }
-  while(g_bRunning){
-    render();
-  }
-
-    
-    
-
-    SDL_Quit();
-
+  g_game->clean();
     return 0;
 }
