bab2f173559bcae03865ca75ac28681167c257e1
20181290 HanChanHui <78011635+hanchanhui@users.noreply.github.com>
2021-10-29 02:24:47 +0000

    SDL-애니메이션

--- Game.cpp
+++ Game.cpp
@@ -20,14 +20,17 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
     else {
         return false;
     }
-    SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+    SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
     m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
     SDL_FreeSurface(pTempSurface);
 
-    SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+    
     m_sourceRectangle.x=0;
     m_sourceRectangle.y=0;
+    
 
+    m_sourceRectangle.w = 128;
+    m_sourceRectangle.h = 82;
     m_destinationRectangle.w = m_sourceRectangle.w;
     m_destinationRectangle.h = m_sourceRectangle.h;
 
@@ -39,7 +42,7 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
 }
 
 void Game::update(){
-
+  m_sourceRectangle.x=128 * ((SDL_GetTicks() / 100) % 6);
 }
 
 void Game::render(){
